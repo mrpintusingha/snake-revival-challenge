f7fa7428d93aa7ca6cae9952181fa812ab9c58fb
CallLive <callliveai@gmail.com>
2026-09-01 11:14:42 +0530

    chore: cleanup dead code and unused dependencies

--- check2.py
+++
@@ -1,6 +0,0 @@
-﻿with open('src/components/SnakeGame.tsx', 'rb') as f:
-    b = f.read()
-if b'\xe2\x96\xb2' in b: # UP arrow
-    print("UP ARROW IS PRESENT")
-elif b'\xc3\xa2\xe2\x80\x93\xc2\xb2' in b:
-    print("MANGLED ARROW IS PRESENT")
